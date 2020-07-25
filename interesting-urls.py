3072780beeec8b76eb012f187e6fc53877f44bec
Daniel Zajork <dan@Mr-Coffee-Robot.local>
2020-07-25 16:16:41 -0500

    Cleaner URL matching for extensions, added uknown extension feature

--- interesting-urls.py
+++ interesting-urls.py
@@ -2,12 +2,11 @@
 import re
 import sys
 from argparse import ArgumentParser
-from urllib.parse import urlsplit, parse_qs, urlparse, unquote
+from urllib.parse import urlsplit, urlunsplit, parse_qs, urlparse, unquote
 from pathlib import PurePosixPath
 
 
-def has_extension(url) -> bool:
-    extensions = [".cs",
+extensions = [".cs",
                   ".vb",
                   ".cgi",
                   ".pl",
@@ -20,8 +19,34 @@ def has_extension(url) -> bool:
                   ".yml",
                   ".ini",
                   ".md",
-                  ".mkd"
-                  ]
+                  ".mkd",
+                  ".config",
+                  ".conf",
+                  ".cfg",
+                  ".ps1",
+                  ".bat"]
+
+static_extensions = [".js",
+                  ".html",
+                  ".htm",
+                  ".svg",
+                  ".eot",
+                  ".ttf",
+                  ".woff",
+                  ".woff2",
+                  ".png",
+                  ".jpg",
+                  ".jpeg",
+                  ".gif",
+                  ".ico"]
+
+
+def remove_query_params_and_fragment(url):
+    return urlunsplit(urlsplit(url)._replace(query="", fragment=""))
+
+
+def has_extension(url) -> bool:
+    url = remove_query_params_and_fragment(url)
 
     for ext in extensions:
         if url.endswith(ext):
@@ -75,7 +100,12 @@ def has_interesting_query_strings(url) -> bool:
             "src",
             "href",
             "func",
-            "callback"]
+            "callback",
+            "secret",
+            "hash",
+            "java",
+            "jdk",
+            "sdk"]
 
     values = ["http",
               "https",
@@ -102,25 +132,53 @@ def has_interesting_query_strings(url) -> bool:
 
 
 def is_static_file(url) -> bool:
-    extensions = [".js",
-                  ".html",
-                  ".htm",
-                  ".svg",
-                  ".eot",
-                  ".ttf",
-                  ".woff",
-                  ".woff2",
-                  ".png",
-                  ".jpg",
-                  ".jpeg",
-                  ".gif",
-                  ".ico"]
+    url = remove_query_params_and_fragment(url)
 
-    for ext in extensions:
+    for ext in static_extensions:
         if url.endswith(ext):
             return True
 
     return False
+
+
+def is_unknown_extension(url) -> bool:
+    url = remove_query_params_and_fragment(url)
+
+    url_paths = PurePosixPath(
+        unquote(
+            urlparse(
+                url
+            ).path
+        )
+    ).parts
+
+    if not url_paths:
+        return False
+
+    last_element = url_paths[-1]
+
+    # no extension, ends with path
+    if not "." in last_element:
+        return False
+
+    for ext in extensions:
+        if url.endswith(ext):
+            return False
+    for ext in static_extensions:
+        if url.endswith(ext):
+            return False
+
+    return True
+
+
+def find_unknown_extensions(urls) -> None:
+    for url in urls:
+
+        parsed_url = re.search("(?P<url>https?://[^\s]+)", url).group("url")
+        parsed_url = parsed_url.lower().strip('\n')
+
+        if is_unknown_extension(parsed_url):
+            print(parsed_url)
 
 
 def search_urls(urls) -> None:
@@ -145,7 +203,8 @@ def main() -> None:
     Main program
     """
     parser = ArgumentParser()
-    parser.add_argument("-f", "--file", dest="file", help="File containing list of URLs")
+    parser.add_argument("-f", "--file", dest="file", help="File containing list of URLs", required=True)
+    parser.add_argument("-u", "--unknown", dest="unknown", help="Print unknown extensions")
 
     args = parser.parse_args()
 
@@ -153,11 +212,13 @@ def main() -> None:
         parser.print_help()
         exit(1)
 
-    if args.file is not None:
-        with open(args.file, "r") as input_file:
-            urls = input_file.readlines()
+    with open(args.file, "r") as input_file:
+        urls = input_file.readlines()
 
-    search_urls(urls)
+    if args.unknown is not None:
+        find_unknown_extensions(urls)
+    else:
+        search_urls(urls)
 
 
 if __name__ == "__main__":
